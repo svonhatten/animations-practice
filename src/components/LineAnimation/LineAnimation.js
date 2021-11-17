ed31a874a9b529d0537e27487e401b5b392419f9
Sophie Von Hatten <svonhatten@luc.edu>
2021-11-16 21:41:06 -0600

    continue work on notification animation

--- src/components/LineAnimation/LineAnimation.js
+++ src/components/LineAnimation/LineAnimation.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 import './styles.css';
 
 const LineAnimation = () => {
+
+    const buttonClick = () => {
+        console.log('you clicked me');
+
+    }
+
     return (
         <div className={'root'}>
-            <div className={'lineContainer'}>
-                <div className={'point'}>
-                    
+            <div className={'buttonContainer'}>
+                <Button variant="contained" color="default" size="large" className={'button'} onClick={() => buttonClick()}>Click Me, See What Happens!</Button>
+            </div>
+            <div className={'footer'}>
+                <div className={'lineContainer'}>
+                    <div className={'notif'}>
+                        <p className={'notifText'}>Hi! I'm a notification for you to see!</p>
+                    </div>
                 </div>
             </div>
         </div>
