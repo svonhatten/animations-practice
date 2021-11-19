ce2288feab1003bd811af2496f8d7f9fe57e3a90
Sophie Von Hatten <svonhatten@luc.edu>
2021-11-18 21:24:57 -0600

    get loading animation going

--- src/components/LoadingAnimation/LoadingAnimation.js
+++ src/components/LoadingAnimation/LoadingAnimation.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './styles.css';
 
 //either paint scribble animation, or perhaps a drawn square in
 //the shape of a frame for an art piece, make it spin?
 const LoadingAnimation = () => {
     return (
-        <div>This is for loading...</div>
+        <div className={'root'}>
+            <div className={'box'}></div>
+            <div className={'text'}>This is for loading...</div>
+        </div>
     )
 };
 
