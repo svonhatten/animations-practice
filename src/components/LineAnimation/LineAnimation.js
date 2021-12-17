ee1dfebc28f29c7cae2fcfe613625d05df5d15a9
Sophie Von Hatten <svonhatten@luc.edu>
2021-12-16 19:35:30 -0600

    update notif page

--- src/components/LineAnimation/LineAnimation.js
+++ src/components/LineAnimation/LineAnimation.js
@@ -1,13 +1,36 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import { useNavigate } from 'react-router-dom';
+import { Button, Zoom } from '@material-ui/core';
+import { ChevronLeft } from '@material-ui/icons';
+import { makeStyles } from '@material-ui/core/styles';
 import './styles.css';
+
+const useStyles = makeStyles({
+    button: {
+        order: 0,
+        flex: '0 1 auto',
+        alignSelf: 'center',
+        backgroundColor: '#1c1b1b',
+        border: '2px solid transparent',
+        borderImage: 'linear-gradient(to bottom right, #b827fc 0%, #2c90fc 25%, #b8fd33 50%, #fec837 75%, #fd1892 100%)',
+        borderImageSlice: 1,
+        color: 'white',
+        fontWeight: 'bold',
+    }
+})
 
 const LineAnimation = () => {
 
+    const classes = useStyles();
+
+    //for when the user wants to go back to the prev page
+    const navigate = useNavigate();
+
+    // state tracking for animation
     const [animate, setAnimate] = React.useState(false);
 
+    // handler for when the button is clicked to show the notification
     const buttonClick = () => {
-        console.log('you clicked me');
         setAnimate(true);
         document.getElementById('notif').className = 'notif';
 
@@ -15,13 +38,21 @@ const LineAnimation = () => {
 
     return (
         <div className={'root'}>
-            <div className={'buttonContainer'}>
-                <Button variant="contained" color="default" size="large" className={'button'} onClick={() => buttonClick()}>Click Me, See What Happens!</Button>
+            <div className={'backButtonContainer'}>
+                <Button onClick={() => navigate('/')}>
+                    <ChevronLeft size={'large'} className={'backIcon'}/> 
+                    <div className={'backIcon'}>Back to Home</div>
+                </Button>
             </div>
+            <Zoom in={true}>
+                <div className={'buttonContainer'}>
+                    <Button size="large" className={classes.button} onClick={() => buttonClick()}>Click Me, See What Happens!</Button>
+                </div>
+            </Zoom>
             <div className={'footer'}>
                 <div className={'lineContainer'}>
                     <div id="notif" className={animate ? 'notif' : null}>
-                        {animate ? <p className={animate ? 'notifText' : null}>Hi! I'm a notification for you to see!</p> : null}
+                        {animate ? <p className={animate ? 'notifText' : null}> Submission Successful </p> : null}
                     </div>
                 </div>
             </div>
