ffe14301ec041d2823b44836095cacb34c6eccc9
Sophie Von Hatten <svonhatten@luc.edu>
2021-12-16 19:35:55 -0600

    update loading page

--- src/components/LoadingAnimation/LoadingAnimation.js
+++ src/components/LoadingAnimation/LoadingAnimation.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import { Zoom } from '@material-ui/core';
+import { useNavigate } from 'react-router-dom';
+import { Button, Zoom } from '@material-ui/core';
+import { ChevronLeft } from '@material-ui/icons';
 import './styles.css';
 
-//either paint scribble animation, or perhaps a drawn square in
-//the shape of a frame for an art piece, make it spin?
 const LoadingAnimation = () => {
+
+    //for when the user wants to go back to the prev page
+    const navigate = useNavigate();
+
     return (
         <div className={'root'}>
-            <Zoom in={true}>
-                <div className={'box'}></div>
-            </Zoom>
-            <Zoom in={true}>    
-                <div className={'text'}>This is for loading...</div>
-            </Zoom>
+            <div className={'backButtonContainer'}>
+                <Button onClick={() => navigate('/')}>
+                    <ChevronLeft size={'large'} className={'backIcon'}/> 
+                    <div className={'backIcon'}>Back to Home</div>
+                </Button>
+            </div>
+            <div className={'loadingContainer'}>
+                <Zoom in={true}>
+                    <div className={'box'}></div>
+                </Zoom>
+                <Zoom in={true}>    
+                    <div className={'text'}>Loading...</div>
+                </Zoom>
+            </div>
         </div>
     )
 };
